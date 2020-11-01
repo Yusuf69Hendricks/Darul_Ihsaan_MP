30e76ee71af67c92cb5799a12b91ad251e02364a
Yusuf69Hendricks <yusuf69hendricks@gmail.com>
2020-11-01 08:05:37 -0800

    added navbar text color change

--- main/main.js
+++ main/main.js
@@ -21,15 +21,15 @@ var myNav= document.getElementById('myTopnav');
 window.onscroll = function () { 
     "use strict";
     if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200 ) {
-        myNav.classList.add("nav-colored");
-        myNav.classList.add("nav-font");
+        myNav.classList.add("nav-colored","navfont");
         myNav.classList.remove("nav-transparent");
     } 
     else {
         myNav.classList.add("nav-transparent");
-        myNav.classList.remove("nav-colored");
-        myNav.classList.remove("nav-font");
+        myNav.classList.remove("nav-colored","navfont");
     }
 };
+
+
 // COLOR CHANGE 
 // NAVBAR 

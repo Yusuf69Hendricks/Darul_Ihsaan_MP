7f280aca7027672231165a4aec5579019fde15b2
Yusuf69Hendricks <yusuf69hendricks@gmail.com>
2020-11-03 04:21:37 -0800

    changed colors and pictures

--- main/main.js
+++ main/main.js
@@ -1,35 +1,36 @@
 // NAVBAR
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
 
-  function openNav() {
-    document.getElementById("mySidenav").style.width = "100%";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
 
 //   COLOR CHNAGE
-var myNav= document.getElementById('myTopnav');
-window.onscroll = function () { 
-    "use strict";
-    if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200 ) {
-        myNav.classList.add("nav-colored","navfont");
-        myNav.classList.remove("nav-transparent");
-    } 
-    else {
-        myNav.classList.add("nav-transparent");
-        myNav.classList.remove("nav-colored","navfont");
-    }
+var myNav = document.getElementById("myTopnav");
+window.onscroll = function () {
+  "use strict";
+  if (
+    document.body.scrollTop >= 150 ||
+    document.documentElement.scrollTop >= 150
+  ) {
+    myNav.classList.add("nav-colored", "navfont");
+    myNav.classList.remove("nav-transparent");
+  } else {
+    myNav.classList.add("nav-transparent");
+    myNav.classList.remove("nav-colored", "navfont");
+  }
 };
 
-
-// COLOR CHANGE 
-// NAVBAR 
+// COLOR CHANGE
+// NAVBAR
